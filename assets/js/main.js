2aba0576c676817eaa3779880a51295ab55970be
Marcos Antonio Cerqueira <marcosfullstack@gmail.com>
2019-04-30 23:32:43 -0300

    adding menu and search

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,26 +20,59 @@ document.addEventListener('DOMContentLoaded', function () {
   var menu = document.getElementById('menu');
   var search = document.getElementById('search'); 
   var form_search= document.getElementById('form-search'); 
-  var search_input= document.querySelectorAll('.search_input');   
+  var windowWidth = window.innerWidth;
+  var tela = document.getElementById('width'); 
   var open_search = false;
+  var button_input = document.getElementById('button_input'); 
+  var search_input = document.getElementById('search_input'); 
+
+ 
+
   forward.onclick = function () {
-      //pequena lógica para avança o scroll
-      if(post.scrollLeft == 400 ){
-      //se o scroll alcanço 400px 
-        post.scrollLeft = 0 - post.scrollLeft ;
-        back.style.display='none';
-      }else{
-      //avança o scroll em 200px
-        post.scrollLeft +=200;
-        back.style.display='block';
-      }
+    let valor =0;
+    let avança=0;
+    //pequena lógica para avança o scroll sabendo o tamanho da tela
+    if (windowWidth == 1440 ){
+      valor = 1977;
+      avança = 310;
+    }
+    else if ( windowWidth == 1280)
+    {
+    valor = 2120;
+    avança = 310;
+    }
+    else if ( windowWidth <= 460)
+    {
+    valor = 3003;
+    avança = 330;
+    }
+
+    if ( post.scrollLeft == valor )
+    {
+    //se o scroll alcanço 400px 
+      post.scrollLeft = 0 - post.scrollLeft ;
+      back.style.display='none';
+    }
+    else
+    {
+     //avança o scroll em 200px
+      post.scrollLeft +=avança;
+      back.style.display='block';
+      console.log( post.scrollLeft);
+    }
+  
   };
 
   back.onclick = function () {
     //retrocede o scroll em 200px
-      post.scrollLeft -=200;
+      post.scrollLeft -=300;
+      if ( post.scrollLeft == 0 ) 
+      {
+        back.style.display='none';
+      }
   };
 
+ 
 
   btn_menu_close.onclick = function () {
       //quero  fazer algo 
@@ -61,6 +94,8 @@ document.addEventListener('DOMContentLoaded', function () {
     form_search.style.padding= '0';
     form_search.style.opacity= '0';
     open_search = false;
+    button_input.style.display='none';
+    search_input.style.display='none';
 
     menu.style.display = 'flex';
     menu.style.width = '100%';
@@ -74,10 +109,13 @@ document.addEventListener('DOMContentLoaded', function () {
 };  
 
 form_search.style.display = 'none';
+
 // lógica para abrir o search com onclick
  search.onclick = function(){
+
    // controle do search
-   menu.style.width = '0px';
+   //estilização padrão
+      menu.style.width = '0px';
       menu.style.opacity = '0';
       menu.style.transition= 'all .5s';
       btn_menu_close.style.opacity= '0';
@@ -85,20 +123,28 @@ form_search.style.display = 'none';
       btn_menu_open.style.display = 'flex';
       btn_menu_close.style.display = 'none';
       btn_menu_open.style.opacity= '1'; 
-  if ( open_search === false ){
+      
 
+  if ( open_search === false ){
+   // se menu estiver fechado ele abre
     form_search.style.animation = 'SlideDown 1s'
     form_search.style.display = 'flex';
     form_search.style.height= 'auto';
     form_search.style.padding= '2rem';
     form_search.style.opacity= '1';
+    button_input.style.display='block';
+    search_input.style.display='block';
     open_search = true;
+    
   }
   else{
+  // senão ele fecha
     form_search.style.display = 'flex';
     form_search.style.height= '0px';
     form_search.style.padding= '0';
     form_search.style.opacity= '0';
+    button_input.style.display='none';
+    search_input.style.display='none';
     open_search = false;
   }
 
@@ -106,5 +152,15 @@ form_search.style.display = 'none';
  }
  
 
+
 }, false);
 
+function tela(tela ){
+  var windowWidth = window.innerWidth;
+  var tela = document.getElementById('width'); 
+  tela.innerHTML = windowWidth;
+}
+tela();
+window.addEventListener('resize', function(){
+tela();
+});
